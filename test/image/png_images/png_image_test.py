bcbe28e299bbfdf8b1728478ed20d636f881ba99
Dave Ankin <alexanderankin@gmail.com>
2017-05-02 06:20:27 -0400

    fixed test reproduceability: image order

--- test/image/png_images/png_image_test.py
+++ test/image/png_images/png_image_test.py
@@ -33,6 +33,7 @@ images = [relative_path_to(f) for f
           in os.listdir(relative_path_to('.'))
           if f.endswith(".png")
             and os.path.basename(f) not in not_supported]
+images.sort()
 
 # Load images before entering test
 for image in images:
@@ -64,11 +65,14 @@ class InsertPNGSuiteMemory(unittest.TestCase):
 
     test_hash = calculate_hash_of_file(outfile)
 
+    # ordered the images for reproduceability
+    self.assertEqual(test_hash, "bd829f7d19da40870a1fbe6571f1f7ac")
+
     # i have no idea why this is different from the one below
     # this is a hash of a pdf file visually indistinguishable from the prev.
     # version of this file. The only difference is that the in memory stuff
     # is happening outside of the class, for timing purposes.
-    self.assertEqual(test_hash, "46c48893d62f3f5878a02ade19a75e07")
+    # self.assertEqual(test_hash, "46c48893d62f3f5878a02ade19a75e07")
 
     # new hash after skipping the ones not supported by default
     # self.assertEqual(test_hash, "652c74f2fa6b12f0c80eebd75b2a2b18")
@@ -81,19 +85,6 @@ class InsertPNGSuiteFiles(unittest.TestCase):
   def test_insert_png_files(self):
     pdf = fpdf.FPDF(unit = 'pt')
     pdf.compress = False
-
-    images = [relative_path_to(f) for f
-              in os.listdir(relative_path_to('.'))
-              if f.endswith(".png")]
-
-    not_supported = [
-      "e59ec0cfb8ab64558099543dc19f8378.png",  # Interlacing not supported:
-      "6c853ed9dacd5716bc54eb59cec30889.png",  # 16-bit depth not supported:
-      "ac6343a98f8edabfcc6e536dd75aacb0.png",  # Interlacing not supported:
-      "93e6127b9c4e7a99459c558b81d31bc5.png",  # Interlacing not supported:
-      "18f9baf3834980f4b80a3e82ad45be48.png",  # Interlacing not supported:
-      "51a4d21670dc8dfa8ffc9e54afd62f5f.png",  # Interlacing not supported:
-    ]
 
     for image in images:
       if os.path.basename(image) in not_supported:
@@ -110,7 +101,10 @@ class InsertPNGSuiteFiles(unittest.TestCase):
     # print(calculate_hash_of_file(outfile))
 
     test_hash = calculate_hash_of_file(outfile)
-    self.assertEqual(test_hash, "4f65582566414202a12ed86134de10a7")
+    # ordered the images for reproduceability
+    self.assertEqual(test_hash, "5086e833d6bf03959d83cd30f3b65ae5")
+
+    # self.assertEqual(test_hash, "4f65582566414202a12ed86134de10a7")
     os.unlink(outfile)
 
 if __name__ == '__main__':
